0b7b886a2ba3389720a2b7dc8e874f5cc3f3db80
Patryk <brzepat@gmail.com>
2024-01-18 19:47:25 +0100

    Add all categories on app init

--- src/main/java/com/example/osk/OskApplication.java
+++ src/main/java/com/example/osk/OskApplication.java
@@ -1,7 +1,14 @@
 package com.example.osk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class OskApplication {
@@ -9,6 +16,16 @@ public class OskApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(OskApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initDatabase(JdbcTemplate jdbcTemplate) {
+        return args -> {
+            ClassPathResource resource = new ClassPathResource("dbPatches/INSERT_ALL_CATEGORIES.SQL");
+
+            ResourceDatabasePopulator populator = new ResourceDatabasePopulator(resource);
+            populator.execute(Objects.requireNonNull(jdbcTemplate.getDataSource()));
+        };
     }
 
 }
